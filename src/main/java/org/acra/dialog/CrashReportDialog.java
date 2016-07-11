27a0805c1bce500f733ed11341bfb938e81dba87
Karn <kpatel@general-devices.com>
2016-07-11 11:08:45 -0400

    - 4.9.2 - Converted to appcompat

--- src/main/java/org/acra/dialog/CrashReportDialog.java
+++ src/main/java/org/acra/dialog/CrashReportDialog.java
@@ -1,12 +1,12 @@
 package org.acra.dialog;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.text.InputType;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
